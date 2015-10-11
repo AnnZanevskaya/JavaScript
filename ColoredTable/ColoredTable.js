3de10b0c3733ef55ff5b3c705fea46f222db86e2
AnnZanevskaya <ann.zanevskaya7@gmail.com>
2015-10-11 16:18:42 +0300

    Update ColoredTable.js

--- ColoredTable/ColoredTable.js
+++ ColoredTable/ColoredTable.js
@@ -10,17 +10,25 @@ function tableCreate(){
       td.style.border = '1px solid black'; 
       td.style.width = '40px';
       td.style.height = '40px';
+      
       td.style.background = 'red';
+      //or
+      //td.classList.add("firstColor"); + description of classes  firstColor/anotherColor in CSS
     }
   }
   body.appendChild(table);
 
   table.addEventListener('click', function(e) {
     var target = e.target;
+    
     if(target.style.background.includes('red'))
        target.style.background = 'blue';   
     else  
        target.style.background = 'red'; 
+       
+    //or 
+    //target.classList.toggle("anotherColor");
+   
   });
 }
 tableCreate();
