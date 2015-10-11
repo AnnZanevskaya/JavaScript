58c8ba70b55cf30999797b5724bb72bf6ec7e88a
Ann Zanevskaya <ann.zanevskaya7@gmail.com>
2015-10-11 14:33:30 +0300

    implement table creation

---
+++ ColoredTable/ColoredTable.js
@@ -0,0 +1,19 @@
+function tableCreate(){
+  var body = document.body,
+  table = document.createElement('table');
+  table.style.border = '1px solid black';
+
+  for(var i = 0; i < 9; i++){
+    var tr = table.insertRow();
+    for(var j = 0; j < 9; j++){
+      var td = tr.insertCell();               
+      td.style.border = '1px solid black'; 
+      td.style.width = '40px';
+      td.style.height = '40px';
+      td.style.background = 'red';
+    }
+  }
+  body.appendChild(table);
+}
+tableCreate();
+
