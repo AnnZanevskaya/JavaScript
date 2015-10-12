2ae908d7ff91d7cb1d3830635ebe4900accfd3c2
Ann Zanevskaya <ann.zanevskaya7@gmail.com>
2015-10-12 10:57:45 +0300

    files for carousel

---
+++ Carousel/CarouselJS.js
@@ -0,0 +1,29 @@
+// JavaScript Document
+var getItmes = 
+            [{
+                "img_src" : "img/gal1.jpg",
+                "description" : "Text 1"
+            },
+            {
+                "img_src" : "img/gal2.jpg",
+                "description" : "Text 2"
+            },
+            {
+                "img_src" : "img/gal3.jpg",
+                "description" : "Text 3"
+            },
+            {
+                "img_src" : "img/gal4.jpg",
+                "description" : "Text 4"
+            },
+            {
+                "img_src" : "img/gal5.jpg",
+                "description" : "Text 5"
+            },
+            {
+                "img_src" : "img/gal6.jpg",
+                "description" : "Text 6"
+            }];
+
+var Carousel = function(itemCount, viewport, itemWidth, items) {
+}
