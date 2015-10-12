bfa487f665beb03c4db6eedb92081f6b2a8db9d4
Ann Zanevskaya <ann.zanevskaya7@gmail.com>
2015-10-12 10:44:06 +0300

    get ready for carousel

--- Carousel/Carousel.js
+++ Carousel/Carousel.js
@@ -1,35 +1,29 @@
 // JavaScript Document
 var getItmes = 
             [{
-                "img_src" : "img/DSCN0027.jpg",
+                "img_src" : "img/gal1.jpg",
                 "description" : "Text 1"
             },
             {
-                "img_src" : "img/DSCN0036.jpg",
+                "img_src" : "img/gal2.jpg",
                 "description" : "Text 2"
             },
             {
-                "img_src" : "img/DSCN0118.jpg",
+                "img_src" : "img/gal3.jpg",
                 "description" : "Text 3"
             },
             {
-                "img_src" : "img/DSCN0321a.jpg",
+                "img_src" : "img/gal4.jpg",
                 "description" : "Text 4"
             },
             {
-                "img_src" : "img/IMG_2365.jpg",
+                "img_src" : "img/gal5.jpg",
                 "description" : "Text 5"
             },
             {
-                "img_src" : "img/PlanePair2.jpg",
+                "img_src" : "img/gal6.jpg",
                 "description" : "Text 6"
             }];
 
 var Carousel = function(itemCount, viewport, itemWidth, items) {
-	
-	
-	
-	
-	
-	
-	}
+}
