05ad54c9a98a12bbf3bdfcede8a9f0c676b6a0a3
Ann Zanevskaya <ann.zanevskaya7@gmail.com>
2015-10-12 10:50:59 +0300

    files for carousel

--- Carousel/CarouselJS.js
+++ Carousel/CarouselJS.js
@@ -1,27 +1,27 @@
 // JavaScript Document
 var getItmes = 
             [{
-                "img_src" : "img/gal1.jpg",
+                "img_src" : "imgs/gal1.jpg",
                 "description" : "Text 1"
             },
             {
-                "img_src" : "img/gal2.jpg",
+                "img_src" : "imgs/gal2.jpg",
                 "description" : "Text 2"
             },
             {
-                "img_src" : "img/gal3.jpg",
+                "img_src" : "imgs/gal3.jpg",
                 "description" : "Text 3"
             },
             {
-                "img_src" : "img/gal4.jpg",
+                "img_src" : "imgs/gal4.jpg",
                 "description" : "Text 4"
             },
             {
-                "img_src" : "img/gal5.jpg",
+                "img_src" : "imgs/gal5.jpg",
                 "description" : "Text 5"
             },
             {
-                "img_src" : "img/gal6.jpg",
+                "img_src" : "imgs/gal6.jpg",
                 "description" : "Text 6"
             }];
 
