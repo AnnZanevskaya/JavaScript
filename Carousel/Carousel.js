465ee39322206cc5bc288439a06f172efe052643
Ann Zanevskaya <ann.zanevskaya7@gmail.com>
2015-10-12 10:40:32 +0300

    get ready for carousel

---
+++ Carousel/Carousel.js
@@ -0,0 +1,35 @@
+// JavaScript Document
+var getItmes = 
+            [{
+                "img_src" : "img/DSCN0027.jpg",
+                "description" : "Text 1"
+            },
+            {
+                "img_src" : "img/DSCN0036.jpg",
+                "description" : "Text 2"
+            },
+            {
+                "img_src" : "img/DSCN0118.jpg",
+                "description" : "Text 3"
+            },
+            {
+                "img_src" : "img/DSCN0321a.jpg",
+                "description" : "Text 4"
+            },
+            {
+                "img_src" : "img/IMG_2365.jpg",
+                "description" : "Text 5"
+            },
+            {
+                "img_src" : "img/PlanePair2.jpg",
+                "description" : "Text 6"
+            }];
+
+var Carousel = function(itemCount, viewport, itemWidth, items) {
+	
+	
+	
+	
+	
+	
+	}
