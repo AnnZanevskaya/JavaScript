1bf5aac96521904b56acfd1ba9d3f44119fe4819
AnnZanevskaya <ann.zanevskaya7@gmail.com>
2015-10-12 11:53:58 +0300

    Update BinaryTree.js

--- BinaryTree/BinaryTree.js
+++ BinaryTree/BinaryTree.js
@@ -77,7 +77,7 @@ BinaryTree.prototype.DeleteNode = function(value){
       //if two children exist
       else if (node.left && node.right){
         node.right.parent = node.parent;
-        node.right.left = node.left;
+        node.right.right = node.left; //changing for right side
         node.left.parent = node.right;
         if(node.parent.left === node)
            node.parent.left = node.right;
